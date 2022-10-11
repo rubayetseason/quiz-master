d8b5afcb06a8d4af061f48a0b0bc6025c37300c4
rubayetseason <season.ss2001@gmail.com>
2022-10-11 21:12:35 +0600

    comparison done

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const Option = ({ option, correctAnswer }) => {
+    const ans = (clicked) => {
+       if(clicked === correctAnswer){
+        console.log('correct');
+       }
+       else{
+        console.log('wrong');
+       }
+    }
     return (
         <div>
-            <button className='bg-blue-600 hover:bg-blue-800 text-white my-1 p-2 w-2/4 rounded font-medium'>{option}</button>
+            <button className='bg-blue-600 hover:bg-blue-800 text-white my-1 p-2 w-2/4 rounded font-medium' onClick={() => ans(option)}>{option}</button>
         </div>
     );
 };
