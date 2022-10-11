372b3cb38bf9fe45b25f23b5720fa1f139c3c197
rubayetseason <season.ss2001@gmail.com>
2022-10-11 23:41:20 +0600

    rechart left

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -9,19 +9,6 @@ const Statistics = () => {
     return (
         <div>
             <hr className='border-2 border-blue-600 my-9 mx-auto w-9/12' />
-
-            <div className='question'>
-                <div className='question-container'>
-                    <ResponsiveContainer width="100%" height="100%">
-                        <LineChart width={500} height={400} data={bardata}>
-                            <Line type="monotone" dataKey="total" stroke='#82ca9d' />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip></Tooltip>
-                        </LineChart>
-                    </ResponsiveContainer>
-                </div>
-            </div>
         </div>
     );
 };
