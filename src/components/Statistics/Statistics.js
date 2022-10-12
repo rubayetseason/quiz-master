6fda26d5d083b67f47364935f8af66b2cac34475
rubayetseason <season.ss2001@gmail.com>
2022-10-12 12:36:25 +0600

    rechart done

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,14 +1,27 @@
+import { Tooltip } from 'flowbite-react';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
-import './Statistics.css'
+import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 const Statistics = () => {
     const data = useLoaderData();
     const bardata = data.data;
     console.log(bardata);
     return (
         <div>
-            <hr className='border-2 border-blue-600 my-9 mx-auto w-9/12' />
+            <hr className='border-2 border-blue-600 my-8 mx-auto w-3/4' />
+<h1 className='text-2xl font-bold'>
+    Here is a <span className='text-blue-600'> chart on total quiz</span> available in our website.
+</h1>
+      <div className='flex justify-center'>
+        <ResponsiveContainer width="95%" height={400}>
+        <LineChart width={500} height={400} data={bardata}>
+           <Line type="monotone" dataKey="total" stroke='#82ca9d' />
+           <XAxis dataKey="name" />
+           <YAxis/>
+           <Tooltip></Tooltip>
+            </LineChart>
+        </ResponsiveContainer>
+        </div>      
         </div>
     );
 };
